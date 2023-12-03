973293859e7ee68061b16a8729f485c190e9cba3
arantruslove <125707012+arantruslove@users.noreply.github.com>
2023-12-03 22:18:13 +0000

    Removing unnecessary tests.

--- jacobi_test.py
+++ jacobi_test.py
@@ -63,39 +63,7 @@ solutions_2d = solutions.reshape((10, 10))
 df_solutions = pd.DataFrame(solutions_2d, columns=np.arange(10), index=np.arange(10))
 
 print(df_solutions)
-# %% Single iteration of test case 1
 
-HEIGHT = 10
-WIDTH = 10
-SOURCE_TERM = 0
-LEFT_BC = 2
-RIGHT_BC = 2
-BOTTOM_BC = 3
-TOP_BC = 3
-STEP_WIDTH = 1
-STOPPING_CONDITION = 1e-10
-MAX_ITERATIONS = 10000
-
-prev_iteration = np.full((WIDTH, HEIGHT), 1)
-
-iteration = jc.jacobi_poisson_iteration(
-    prev_iteration,
-    SOURCE_TERM,
-    LEFT_BC,
-    RIGHT_BC,
-    BOTTOM_BC,
-    TOP_BC,
-    STEP_WIDTH,
-)
-iteration_2d = iteration.reshape((WIDTH, HEIGHT))
-
-# Create the DataFrame from the 2D array
-# Here, you can define your own row and column labels if needed
-df_iteration = pd.DataFrame(
-    iteration_2d, columns=np.arange(WIDTH), index=np.arange(HEIGHT)
-)
-
-print(df_iteration)
 # %% Test Case 2
 # Define the dimensions of the DataFrame
 width = 10  # Width of the grid
@@ -123,7 +91,7 @@ top_bcs = np.full(WIDTH, 36)
 T_GUESS = 0
 STEP_WIDTH = 1
 STOPPING_CONDITION = 1e-10
-MAX_ITERATIONS = 100000
+MAX_ITERATIONS = 0
 
 solutions = jc.jacobi_poisson_solve(
     HEIGHT,
@@ -144,41 +112,6 @@ solutions += offset
 
 solutions = np.flip(solutions, axis=0)
 
-# %% Test Case 2 for a single iteration
-HEIGHT = 10
-WIDTH = 10
-SOURCE_TERM = 6
-x_dir_bcs = np.array([0, 4, 8, 12, 16, 20, 24, 28, 32, 36])
-y_dir_bcs = np.array([0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
-STEP_WIDTH = 1
-STOPPING_CONDITION = 1e-10
-MAX_ITERATIONS = 10000
-
-prev_iteration = np.full((WIDTH, HEIGHT), 0)
-
-
-for i in range(100):
-    prev_iteration = jc.jacobi_poisson_iteration(
-        prev_iteration,
-        SOURCE_TERM,
-        x_dir_bcs,
-        x_dir_bcs,
-        y_dir_bcs,
-        y_dir_bcs,
-        STEP_WIDTH,
-    )
-
-iteration = prev_iteration
-
-iteration_2d = iteration.reshape((WIDTH, HEIGHT))
-
-# Create the DataFrame from the 2D array
-# Here, you can define your own row and column labels if needed
-df_iteration = pd.DataFrame(
-    iteration_2d, columns=np.arange(WIDTH), index=np.arange(HEIGHT)
-)
-
-print(df_iteration)
 
 # %% Test Case 3
 # Define the dimensions of the DataFrame
@@ -200,109 +133,15 @@ df = pd.DataFrame(U, columns=y_values, index=x_values)
 print(df)
 # %% Solving Test Case 3
 
-HEIGHT = 10
-WIDTH = 10
-SOURCE_TERM = 0
-left_bcs = np.full(HEIGHT, 0)
-right_bcs = np.full(HEIGHT, 18)
-bottom_bcs = np.full(WIDTH, 0)
-top_bcs = np.full(WIDTH, -18)
-T_GUESS = 0
-STEP_WIDTH = 1
-STOPPING_CONDITION = 1e-10
-MAX_ITERATIONS = 1000000
-
-solutions = jc.jacobi_poisson_solve(
-    HEIGHT,
-    WIDTH,
-    SOURCE_TERM,
-    left_bcs,
-    right_bcs,
-    bottom_bcs,
-    top_bcs,
-    T_GUESS,
-    STEP_WIDTH,
-    STOPPING_CONDITION,
-    MAX_ITERATIONS,
-)
-
-offset = 5 - solutions[0][0]
-solutions += offset
-
-solutions = np.flip(solutions, axis=1)
-
-# %% Test Case 3 for a single iteration
-HEIGHT = 10
-WIDTH = 10
-SOURCE_TERM = 0
-left_bcs = np.full(HEIGHT, 0)
-right_bcs = np.full(HEIGHT, 18)
-bottom_bcs = np.full(WIDTH, 0)
-top_bcs = np.full(WIDTH, -18)
-STEP_WIDTH = 1
-STOPPING_CONDITION = 1e-10
-MAX_ITERATIONS = 10000
-
-prev_iteration = np.full((WIDTH, HEIGHT), 0)
-
-
-for i in range(1):
-    prev_iteration = jc.jacobi_poisson_iteration(
-        prev_iteration,
-        SOURCE_TERM,
-        bcs,
-        bcs,
-        -bcs,
-        -bcs,
-        STEP_WIDTH,
-    )
-
-iteration = prev_iteration
-
-
-# %% Testing a single iteration of a 10x10 grid to see if it is working as expected
-
-HEIGHT = 10
-WIDTH = 10
-SOURCE_TERM = 0
-left_bcs = np.full(HEIGHT, 2)
-right_bcs = np.full(HEIGHT, 2)
-bottom_bcs = np.full(WIDTH, 3)
-top_bcs = np.full(WIDTH, 3)
-STEP_WIDTH = 1
-STOPPING_CONDITION = 1e-10
-MAX_ITERATIONS = 10000
-
-prev_iteration = np.full((WIDTH, HEIGHT), 1)
-
-iteration = jc.jacobi_poisson_iteration(
-    prev_iteration,
-    SOURCE_TERM,
-    left_bcs,
-    right_bcs,
-    bottom_bcs,
-    top_bcs,
-    STEP_WIDTH,
-)
-iteration_2d = iteration.reshape((WIDTH, HEIGHT))
-
-# Create the DataFrame from the 2D array
-# Here, you can define your own row and column labels if needed
-df_iteration = pd.DataFrame(
-    iteration_2d, columns=np.arange(WIDTH), index=np.arange(HEIGHT)
-)
-
-print(df_iteration)
-
 # %% Testing the Jacobi method Poisson equation solver
 
 THERMAL_CONDUCTIVITY = 150
 HEIGHT = 1e-3  # in m
 WIDTH = 14e-3  # in m
 STEP_WIDTH = 2e-4  # in m
-SOURCE_TERM = -5e8 / 150
+SOURCE_TERM = -5e6 / 150
 T_A = 20
-T_GUESS = 7000
+T_GUESS = 253
 
 mesh_height = round(HEIGHT / STEP_WIDTH)
 mesh_width = round(WIDTH / STEP_WIDTH)
@@ -324,11 +163,9 @@ temperatures = jc.jacobi_poisson_solve(
     top_bc,
     T_GUESS,
     STEP_WIDTH,
-    5e-8,
-    1000,
+    1e-15,
+    10000,
     jc.natural_bcs,
 )
 
 print(temperatures)
-
-# %%
