3482154bdc3d4d89ec2b1c716dfb014e60cfb34e
arantruslove <125707012+arantruslove@users.noreply.github.com>
2023-12-06 22:34:07 +0000

    Fixing the incorrect k input arguments for the bottom-right corner function.

--- system_test.py
+++ system_test.py
@@ -17,8 +17,7 @@ print(edges)
 
 # %% Testing the Jacobi Poisson solver
 basic_sys = sys.MicroprocessorSystem(2)
-temps = basic_sys.solve_system(3100, 0.0001, 1e-13, 100000)
-
+temps = basic_sys.solve_system(20, 0.0001, 1e-13, 0)
 temps_display = np.flipud(temps.T)
 
 # %%
